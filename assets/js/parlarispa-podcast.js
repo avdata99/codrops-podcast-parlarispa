447009fba93bcbd8d5bc922771f26b87e9fcf57d
audi0slave <laurodaniel@outlook.com>
2019-10-13 14:01:10 -0300

    WIP: Implementación de bootstrap + comienzo de modificación home

--- assets/js/parlarispa-podcast.js
+++ assets/js/parlarispa-podcast.js
@@ -2,25 +2,33 @@
 var base_url = 'https://parlarispa.com';
 var podcast_api_url = '/api/v1/podcasts/podcast/aa2b715c-73aa-412a-9e04-100f60881ffa/';
 
-function get_data() {
+$(document).ready(function() {
+    // Obtenemos el listado de podcasts
+    getPodcastList();
+});
+
+/**
+ * Obtiene el listado de podcasts
+ */
+function getPodcastList() {
     let url = base_url + podcast_api_url;
-    $.get(url, function() {console.log("success");})
-        .done(function(data) {
-            load_page(data);
-            })
-        .fail(function(e, msg) {
-            alert('Error leyendo podcast: ' + msg);
-            })
-        .always(function() {
-            console.log('always');
-        });
-    }
+
+    $.get(url, function() {
+        console.log("success");}
+    ).done(function(data) {
+        load_page(data);
+    }).fail(function(e, msg) {
+        alert('Error leyendo podcast: ' + msg);
+    }).always(function() {
+        console.log('always');
+    });
+}
 
 function load_page(data) {
     $("#psp_main_title").html(data.nombre);
     $("#psp_main_subtitle").html(data.descripcion);
 
-    tpl_episode = `<article class="episode">
+    /*let tpl_episode = `<article class="episode">
                 <h2 class="episode__number">{{ episode_number }}</h2>
                 <div class="episode__media">
                     <a href="episodio.html" class="episode__image" style="background-image: url({{ episode_image_url }});"></a>
@@ -29,7 +37,18 @@ function load_page(data) {
                     <a href="episodio.html?uid={{ episode_uid }}" class="episode__title"><h4>{{ episode_title }}</h4></a>
                     <!-- <p class="episode__description">{{ episode_description }}</p> -->
                 </div>
-                </article>`;
+                </article>`;*/
+
+    let tpl_episode = `
+        <div class='container'>
+            <article class='card mb-3'>
+                <img src='{{ episode_image_url }}' />
+                <div class='card-body'>
+                    <h5 class='card-title'>{{ episode_title }}</h5>
+                    <p class='card-text'>{{ episode_description }}</p>
+                </div>
+            </article>
+        </div>`;
 
     $.each(data.episodios, function(j, episodio){
         let values = {
@@ -47,13 +66,3 @@ function load_page(data) {
         $("#episodes_section").append($article);
     });
 }
-
-
-
-$(function() {
-
-    get_data();    
-
-    
-});
-
